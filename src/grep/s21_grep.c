ee990fe6677aa3b118eddcab00061bd4214194a2
Werlag Deckard <werlagde@su-f3.msk.21-school.ru>
2022-05-25 16:17:51 +0300

    Made cmd parser for grep

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -9,10 +9,14 @@
 
 int main(int argc, char *argv[]) {
     struct grep_state state;
-    llist_t *regexes = initialize_llist();
-    llist_t *files = initialize_llist();
+    struct llist *patterns = initialize_llist();
+    struct llist *files = initialize_llist();
     
     initialize_state(&state);
+    parse_cmd_args(argc - 1, argv + 1, &state, patterns, files);
+
+    free_list(patterns);
+    free_list(files);
     return 0;
 }
 
@@ -27,44 +31,109 @@ void initialize_state(struct grep_state *st) {
     st->flags.s = false;
     st->flags.f = false;
     st->flags.o = false;
+    st->match_count = 0;
+    st->files_to_search = 0;
 }
 
-void parse_cmd_args(int argc, char *argv[], struct grep_state *st, llist_t *regexes, llist_t *files) {
-    if (find_ef_flags(argc, argv)) {
+void parse_cmd_args(int argc, char *argv[], struct grep_state *st, struct llist *patterns, struct llist *files) {
+    parse_regexes(argc, argv, patterns, st);
+    parse_files(argc, argv, files);
+    parse_options(argc, argv, st);
+}
 
+void parse_regexes(int argc, char *argv[], struct llist *patterns, struct grep_state *st) {
+    for (int i = 0; i < argc - 1; i++) {
+        bool is_opt = get_dash_index(argv[i]) == 1;
+        if (is_opt && strchr(argv[i], 'e')) {
+            i++;
+            st->flags.e = true;
+            add_to_llist(patterns, argv[i], false);
+        } else if (is_opt && strchr(argv[i], 'f')) {
+            i++;
+            st->flags.f = true;
+            read_regex_from_file(argv[i], patterns);
+        }
+    }
+    if (!st->flags.e && !st->flags.f)
+        add_to_llist(patterns, argv, false);
+}
+
+void read_regex_from_file(char *filename, struct llist *patterns) {
+    FILE *f = fopen(filename, "r");
+    if (f != NULL) {
+        ssize_t len = 0;
+        while (true) {
+            char *str = NULL;
+            len = getline(&str, 0, f);
+            if (len == -1) {
+                free(str);
+                break;
+            }
+            add_to_llist(patterns, str, true);
+        }
+        if (ferror(f))
+            print_error();
     } else {
-
+        print_error();
     }
 }
 
-bool find_ef_flags(int argc, char *argv[]) {
-    bool result = false;
-    for (size_t i; i < argc && !result; i++) {
-        if (get_dash_index(argv[1]) == 1)
-            result = strpbrk(argv[1], "ef") != NULL;
+void parse_files(int argc, char *argv[], struct llist *files) {
+    for (int i = 0; i < argc; i++) {
+        bool is_opt = get_dash_index(argv[i]) == 1;
+        if (is_opt && strpbrk(argv[i], "ef"))
+            i += 1;
+        else if (!is_opt)
+            add_to_llist(files, argv[i], false);
     }
-    return result;
 }
 
-llist_t* initialize_llist() {
-    llist_t *ll = malloc(sizeof(llist_t));
+void parse_options(int argc, char *argv[], struct grep_state *st) {
+    bool ef_flag_prev = false;
+    for (int i = 0; i < argc; i++) {
+        if (get_dash_index(argv[i]) == 1 && !ef_flag_prev) {
+            char *opt_str = argv[i];
+            while (*opt_str) {
+                st->flags.i |= *opt_str == 'i';
+                st->flags.v |= *opt_str == 'v';
+                st->flags.c |= *opt_str == 'c';
+                st->flags.l |= *opt_str == 'l';
+                st->flags.n |= *opt_str == 'n';
+                st->flags.h |= *opt_str == 'h';
+                st->flags.s |= *opt_str == 's';
+                st->flags.o |= *opt_str == 'o';
+                ef_flag_prev = *opt_str == 'e' || *opt_str == 'f';
+                opt_str++;
+            }
+        }
+    }
+}
+
+struct llist* initialize_llist() {
+    struct llist *ll = malloc(sizeof(struct llist));
     ll->next = NULL;
     ll->data = NULL;
+    ll->heap_used = false;
     return ll;
 }
 
-void add_to_llist(llist_t *ll, void *value) {
-    llist_t *cursor = ll;
+void add_to_llist(struct llist *ll, void *value, bool heap_used) {
+    struct llist *cursor = ll;
     while (cursor->next != NULL)
         cursor = cursor->next;
-    cursor->data = value;
-    cursor->next = malloc(sizeof(llist_t));
+    struct llist *new = malloc(sizeof(struct llist));
+    new->data = value;
+    new->heap_used = heap_used;
+    new->next = NULL;
+    cursor->next = new;
 }
 
-void free_list(llist_t *ll) {
+void free_list(struct llist *ll) {
     while (ll != NULL) {
-        llist_t *tmp = ll;
+        struct llist *tmp = ll;
         ll = ll->next;
+        if (tmp->heap_used)
+            free(tmp->data);
         free(tmp);
     }
 }
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -15,13 +15,25 @@ struct grep_state {
         bool f;
         bool o;
     } flags;
+    size_t files_to_search;
+    size_t match_count;
 };
 
-typedef struct {
-    llist_t *next;
+struct llist {
+    struct llist *next;
     void *data;
-} llist_t;
+    bool heap_used;
+};
 
 void initialize_state(struct grep_state *st);
+void parse_cmd_args(int argc, char *argv[], struct grep_state *st, struct llist *regexes, struct llist *files);
+void parse_regexes(int argc, char *argv[], struct llist *regexes, struct grep_state *st);
+void read_regex_from_file(char *filename, struct llist *regexes);
+void parse_files(int argc, char *argv[], struct llist *files);
+void parse_options(int argc, char *argv[], struct grep_state *st);
+
+struct llist* initialize_llist();
+void add_to_llist(struct llist *ll, void *value, bool heap_used);
+void free_list(struct llist *ll);
 
 #endif  // GREP
